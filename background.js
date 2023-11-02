8f59bec82f4dea9e3a1bd232683ad0bee770ce03
amyfung <af1@wellesley.edu>
2023-11-02 01:17:27 -0400

    Added padding styling

---
+++ background.js
@@ -0,0 +1,5 @@
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Gmail Padding Enhancer has been installed.');
+  });
+  
---
+++ content.js
@@ -0,0 +1,27 @@
+// content.js
+console.log("Gmail Padding Enhancer extension loaded");
+
+function addPaddingToEmailCards() {
+  // Select both types of email cards
+  const cards = document.querySelectorAll('tr.zA.yO, tr.zA.zE');
+
+  cards.forEach(card => {
+    card.classList.add('enhanced-padding');
+  });
+}
+
+// Observe for changes in the DOM as Gmail loads emails dynamically
+const observer = new MutationObserver((mutations) => {
+  for (let mutation of mutations) {
+    if (mutation.type === 'childList') {
+      addPaddingToEmailCards();
+    }
+  }
+});
+
+// Start observing the target node for configured mutations
+const config = { childList: true, subtree: true };
+observer.observe(document, config);
+
+// Call initially in case the DOM is already loaded
+addPaddingToEmailCards();
